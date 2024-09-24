0336ab8f9708381fb5ecf6a53c326233fa11bcc8
LucasNGRE <lucas12negre@gmail.com>
2024-09-24 15:33:33 +0200

    ambient namespaces

---
+++ TypeScript/task_3/js/main.ts
@@ -0,0 +1,20 @@
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from "./interface";
+import { insertRow, deleteRow, updateRow } from "./crud";
+
+const row: RowElement = {
+    firstName: "Guillaume",
+    lastName: "Salva",
+}
+
+const newRowID: RowID = insertRow(row);
+
+const updatedRow: RowElement = {
+    firstName: "Guillaume",
+    lastName: "Salva",
+    age: 23,
+}
+
+updateRow(newRowID, updatedRow);
+
+deleteRow(newRowID);
