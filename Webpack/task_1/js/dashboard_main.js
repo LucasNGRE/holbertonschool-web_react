abeabdf24c3fe5d956706ae04e3b76ac079db69c
LucasNGRE <lucas12negre@gmail.com>
2025-10-10 10:13:31 +0200

    feat: initialize Webpack project with jQuery, Lodash, and basic HTML structure

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document.body).append('<p>Holberton Dashboard</p>');
+$(document.body).append('<p>Dashboard data for the students</p>');
+const button = $('<button>Click here to get started</button>');
+$(document.body).append(button);
+$(document.body).append('<p id="count"></p>');
+$(document.body).append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+function updateCounter() {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+button.on('click', _.debounce(updateCounter, 500));
