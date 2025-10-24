e17df36d7933afcbbe713f12ca8c70dfefdacfe7
LucasNGRE <lucas12negre@gmail.com>
2025-10-24 09:24:57 +0200

    refactor: clean up App component structure and improve logo handling

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import holbertonLogo from './assets/holberton-logo.jpg';
 import './App.css';
-import logo from './assets/holberton-logo.jpg';
-import Notifications from './Notifications';
 import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
 
 function App() {
   return (
-    <div>
+    <>
       <div className="root-notifications">
         <Notifications />
       </div>
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="holberton logo" />
-        <h1>School dashboard</h1>
+        <img
+          className="holberton-logo"
+          src={holbertonLogo}
+          alt="holberton logo"
+        />
+        <h1>School Dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>{getFooterCopy(true)} - {getCurrentYear()}</p>
+        <p>
+          Copyright {getCurrentYear()} - {getFooterCopy()}
+        </p>
       </div>
-    </div>
+    </>
   );
 }
 
