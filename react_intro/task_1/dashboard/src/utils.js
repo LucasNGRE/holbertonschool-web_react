de27c65b7b8306e8f7452d8326292a0d043c7c7c
LucasNGRE <lucas12negre@gmail.com>
2025-10-24 09:20:44 +0200

    refactor: update utility functions to remove export syntax and improve formatting

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -1,7 +1,12 @@
-export function getCurrentYear() {
+function getCurrentYear() {
   return new Date().getFullYear();
 }
 
-export function getFooterCopy(isIndex) {
-  return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return 'Holberton School';
+  }
+  return 'Holberton School main dashboard';
 }
+
+module.exports = { getCurrentYear, getFooterCopy };
