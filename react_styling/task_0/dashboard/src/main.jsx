b3203dc47736749c7e90e219c0259e96ac4aa045
LucasNGRE <lucas12negre@gmail.com>
2025-11-26 09:54:51 +0100

    feat: add main.css for Tailwind and Roboto font styling

--- react_styling/task_0/dashboard/src/main.jsx
+++ react_styling/task_0/dashboard/src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App/App.jsx'
 
+import './main.css'
+
+import "@fontsource/roboto/400.css"  // Regular
+import "@fontsource/roboto/500.css"  // Medium
+import "@fontsource/roboto/700.css"  // Bold
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
